ff50dabc38eb2be3b888dc1bebc3f5fb1ce9a14f
Maden51 <madenon123@gmail.com>
2022-01-06 11:47:34 +0300

    initial setup

---
+++ server.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const koaBody = require('koa-body');
+const WS = require('ws');
+
+const app = new Koa();
+app.use(cors());
+app.use(koaBody(
+    { json: true }
+));
+
+const port = 8080;
+const router = new Router();
+app.use(router.routes()).use(router.allowedMethods());
+const server = http.createServer(app.callback());
+const wsServer = new WS.Server({ server });
+
+const users =  ['user1', 'user2'];
+
+router.get('/test', async (ctx, next) => {
+    ctx.response.body = {
+        status: 'ok',
+        timestamp: Date.now(),
+    };
+});
+
+router.post('/check', async (ctx, next) => {
+    const { userName } = ctx.response.body;
+    console.log(ctx.request);
+    if (users.includes(userName)) {
+        ctx.response.body = {
+            access: false,
+            errorMessage: 'Such username already exists',
+        };
+    } else {
+        users.push(userName);
+        ctx.response.body = {
+            access: true,
+            userName,
+        };
+    }
+});
+
+router.post('/exit', async (ctx, next) => {
+    const { userName } = ctx.response.body;
+    const index = users.findIndex(elem => elem === userName);
+    if (index !== -1) {
+        users.splice(index, 1);
+    } 
+    ctx.response.status = 204;
+});
+
+wsServer.on('connection', (ws, req) => {
+    ws.on('message', msg => {
+        [...wsServer.clients]
+            .filter(c => c.readyState === WS.OPEN)
+            .forEach(c => c.send('to all', msg))
+    });
+    ws.send('welcome', errCallback);
+});
+
+server.listen(port, () => console.log('server started'));
